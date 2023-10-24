d449e69585be74a553e85c698ccfdee0542ae221
user <user@WIN-TE96RD9SUDU>
2023-10-24 23:32:37 +0300

    Add project files.

---
+++ main.cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>
+#include <Windows.h>
+#include <conio.h>
+#include <ctime>
+
+
+using namespace std;
+
+int main()
+{
+	HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
+	
+	srand(time(0));
+	
+	const int size = 10;
+	int arr[size];
+	//for (size_t i = 0; i < size; i++)
+	//{
+	//	arr[i] = -10 + rand()%20;
+	//}
+	for (size_t i = 0; i < 10000000; i++)
+		
+	{
+		SetConsoleTextAttribute(hConsole, 10);
+		cout << char(rand() % 255) << '\t';
+		for (size_t i=0; i < 1000000; i++)
+		{ }
+	}
+	cout << endl;
+	return  0;
+}
